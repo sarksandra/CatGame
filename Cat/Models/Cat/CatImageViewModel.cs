153b4fb1ae06725ec4f029ec1a32b48f96994da0
opilane <opilane@HP-CZC321706B>
2024-10-21 16:31:31 +0300

    good

---
+++ Cat/Models/Cat/CatImageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Cat.Models.Cat
+{
+    public class CatImageViewModel
+    {
+        public Guid ImageID { get; set; }
+        public string ImageTitle { get; set; }
+        public string ImageData { get; set; }
+        public string Image { get; set; }
+        public Guid? CatID { get; set; }
+    }
+}
